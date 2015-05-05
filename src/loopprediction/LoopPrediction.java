6b719fc13dffe732e46b5aa08a5d7f22ac1c8f76
Kanishk Asthana <kani.asthana@gmail.com>
2015-05-05 12:28:21 -0700

    First Commit

---
+++ src/loopprediction/LoopPrediction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loopprediction;
+
+/**
+ *
+ * @author kanis_000
+ */
+public class LoopPrediction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
